71954049bb874f923cb016175d4e344d0fb5d5d9
Brett Drake <recordstuff@hotmail.com>
2024-01-23 03:28:04 -0600

    trying to get angular started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouterOutlet, provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `<h1>Hello World</h1>`,
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: 'app.component.html',
+    styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'brett-angular';
-}
+
+export class AppComponent { }
--- src/main.server.ts
+++ src/main.server.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+
+config.providers.push(provideRouter(routes))
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
